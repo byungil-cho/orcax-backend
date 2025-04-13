079598e3261a6c700d8de672fc43f4f95d52fb92
byungil-cho <jbl01570@hanmail.net>
2025-04-13 12:03:19 +0900

    Update nft.js

--- routes/nft.js
+++ routes/nft.js
@@ -1,1 +1,10 @@
+const express = require('express'); // 익스프레스 부름
+const router = express.Router();    // 라우터 생성
 
+// /test 주소로 들어오면 이걸 보여줌
+router.get('/test', (req, res) => {
+  res.send('NFT route is alive! 🚀');
+});
+
+// 다른 파일에서 불러다 쓸 수 있게 export
+module.exports = router;
