ad18df37e02d76b04194809851fcb3ed2b108293
byungil-cho <jbl01570@hanmail.net>
2025-04-13 12:18:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+// server.js
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import nftRoutes from "./routes/nft.js";
 
-// 서버 앱 만들기
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 미들웨어 등록
 app.use(cors());
 app.use(bodyParser.json());
 
-// NFT 라우터 연결
-const nftRoutes = require('./routes/nft');
-app.use('/api', nftRoutes);
+app.use("/api", nftRoutes);
 
-// 기본 루트 응답
-app.get('/', (req, res) => {
-  res.send('OrcaX Backend is alive!');
+app.get("/", (req, res) => {
+  res.send("OrcaX NFT API 서버 가동 중! 🐳");
 });
 
-// 서버 실행
 app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
+  console.log(`✅ 서버 가동: http://localhost:${PORT}`);
 });
